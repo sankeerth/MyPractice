db67c8fa04a7bbbe4d2a2ce0aa5535d0d50cca92
Sankeerth V S <sankeerthvsriram@gmail.com>
2015-05-04 19:52:58 +0900

    Create next_permutation.c
    Outputs next permutation of the elements in the provided input string

---
+++ Generic/next_permutation.c
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+
+#include "log.h"
+
+#define COMPILE_SINGLE_FILE 0
+
+template <typename Iter>
+bool next_perm(Iter first, Iter last);
+
+#if COMPILE_SINGLE_FILE
+int main()
+{
+    std::string num = "2142";
+    do {
+        LOG(num);
+    }while (next_perm(num.begin(), num.end()));
+}
+#endif
+
+template<typename Iter>
+bool next_perm(Iter first, Iter last)
+{
+    if (first == last)
+        return false;
+    Iter i = first;
+    ++i;
+    if (i == last)
+        return false;
+    i = last;
+    --i;
+
+    for(;;)
+    {
+        Iter ii = i;
+        --i;
+        if (*i < *ii)
+        {
+            Iter j = last;
+            while (!(*i < *--j))
+            {}
+            std::iter_swap(i, j);
+            std::reverse(ii, last);
+            return true;
+        }
+        if (i == first)
+        {
+            std::reverse(first, last);
+            return false;
+        }
+    }
+}
