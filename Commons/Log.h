406257437ecaaf0ed2b972bd66a4c10846b326b0
Sankeerth V S <sankeerthvsriram@gmail.com>
2015-05-04 17:34:26 +0900

    Create Log.h

---
+++ Commons/Log.h
@@ -0,0 +1,9 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include <iostream>
+
+#define LOG(statement) std::cout << statement << std::endl
+#define ERROR(statement) std::cout << statement << std::endl;
+
+#endif // LOG_H
